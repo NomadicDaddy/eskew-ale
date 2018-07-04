04547d457e52a2909105c80cde294a61c0de0520
Phillip Beazley <phillip@beazley.org>
2018-07-04 16:53:48 -0500

    renamed column to suit previously established convention; attempted to add some filters

--- perf/foreign_key_index_and_trust_status.sql
+++ perf/foreign_key_index_and_trust_status.sql
@@ -1,8 +1,12 @@
--- set to blank or null to see fk status for all tables
-
-declare @Table varchar(64) = '' ;
-
 -- foreign key index and trust status
+
+-- upd[180704/pb]: renamed [is_indexed] to [is_not_indexed] to follow [is_not_trusted]
+-- tbf[180704/pb]: added; cond logic of @Show bits is wonky
+
+declare @Table varchar(64) = '' ;	-- leave blank or null to see fk status for all tables
+declare @ShowTrusted bit = 1 ;		-- leave zero if you want just a list of things to fix
+declare @ShowIndexed bit = 1 ;		-- leave zero if you want just a list of things to fix
+
 select distinct
 	[fk_name] = fk.[name],
 	[referencing_schema] = s.[name],
@@ -12,7 +16,7 @@ select distinct
 	[target_table] = object_name(fk.[referenced_object_id]),
 	[target_column] = c.[name],
 	fk.[is_not_trusted],
-	[is_indexed] = case when ic.[object_id] is null then 0 else 1 end,
+	[is_not_indexed] = case when ic.[object_id] is null then 1 else 0 end,
 	[sqlx_idx] = case when ic.[object_id] is null then 'create nonclustered index [IX_' + object_name(fk.[parent_object_id]) + '_' + c.[name] + '] on ' + s.[name] + '.' + QuoteName(object_name(fk.[parent_object_id])) + '(' + QuoteName(c.[name]) + ' asc) with (data_compression = page) ;' else null end,
 	[sqlx_chk] = case when fk.[is_not_trusted] = 1 then 'alter table ' + s.[name] + '.' + QuoteName(object_name(fk.[parent_object_id])) + ' with check check constraint ' + QuoteName(fk.[name]) + ' ;' else null end
 from
@@ -26,8 +30,21 @@ from
 	left outer join sys.index_columns [ic] on ic.[object_id] = fkc.[parent_object_id] and ic.[column_id] = fkc.[parent_column_id] and ic.[index_column_id] = fkc.[constraint_column_id]
 --	left outer join sys.index_columns [ic2] on ic2.[object_id] = fkc.[referenced_object_id] and ic2.[column_id] = fkc.[referenced_column_id] and ic2.[index_column_id] = fkc.[constraint_column_id]
 where
-	fk.[referenced_object_id] = (select [object_id] from sys.tables where [name] = @Table)
-	or (@Table is null or @Table = '')
+	(
+		@Table is null
+		or @Table = ''
+		or fk.[referenced_object_id] = (select [object_id] from sys.tables where [name] = @Table)
+	)
+	and (
+		(
+			@ShowTrusted = 1			-- show all;
+			or fk.[is_not_trusted] = 1	-- else only show untrusted keys to be fixed
+		) or (
+			@ShowIndexed = 1			-- show all;
+			or ic.[object_id] is null	-- else only show unindexed keys to be fixed
+		)
+	)
 order by
 	[referencing_table] asc,
 	fk.[name] asc ;
+go
